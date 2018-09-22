9b1783c289552c2928057551847bd8462eb010c5
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-21 20:24:08 -0600

    added new error path to personal_site and test

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,8 +1,19 @@
 require 'rack'
-require 'pry'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def self.index
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -5,8 +5,14 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
+  end
+
+  def test_404_status_code_is_recieved_when_visting_a_file_that_doesnt_exist
+    visit 'kljf'
+
+    assert_equal 404, page.status_code
+    assert page.has_content?("Page not found.")
   end
 end
