746d930151218f840f0c5ff8ba027f4622d0d739
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-21 19:35:38 -0600

    passed test for personal site and added git ignore

---
+++ test/features/user_sees_a_homepage_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+require 'pry'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+
+    save_and_open_page
+    assert page.has_content?("Welcome!")
+    assert_equal 200, page.status_code
+  end
+end
